503f76f583ef79db444b7cc4f8dad63b75e9598f
tvo015 <77404780+tvo015@users.noreply.github.com>
2021-02-26 13:34:39 -0800

    Working R script

---
+++ R for SATSA_improved.R
@@ -0,0 +1,6 @@
+install.packages("tidyverse") #If not installed
+#Load packages
+library(tidyverse)
+paths <- c("https://raw.githubusercontent.com/jennybc/lotr-tidy/master/data/The_Fellowship_Of_The_Ring.csv",
+           "https://raw.githubusercontent.com/jennybc/lotr-tidy/master/data/The_Two_Towers.csv",
+           "SATSA_datascience.csv")
